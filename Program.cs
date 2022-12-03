b95370f6cf370a5bd3287441f6479838bc68b586
micadevcon <micadevcon@mail.ru>
2022-12-03 18:26:53 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Find_The_Parity_Outlier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
