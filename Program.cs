57a3daa65608cae70d39eebbe42b3da6b68d5c6e
micadevcon <micadevcon@mail.ru>
2022-12-04 22:12:21 +0400

    final

--- Program.cs
+++ Program.cs
@@ -6,7 +6,38 @@ namespace Find_The_Parity_Outlier
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] integers = new int[] { 2, 6, 8, -10, 3 };
+            Console.WriteLine(Find(integers));
+        }
+        public static int Find(int[] integers)
+        {
+            int parityNumber;
+            if (parityLine(integers))
+                parityNumber = 1;
+            else
+                parityNumber = 0;
+
+            for (int i = 0; i < integers.Length; i++)
+            {
+                if (integers[i] % 2 == parityNumber)
+                    return integers[i];   
+            }
+            return -1;
+
+        }
+        public static bool parityLine(int[] integers)
+        {
+            int length = 3;
+            int parityNumbers = 0;
+            for (int i = 0; i < length; i++)
+            {
+                if (integers[i]%2==0)
+                    parityNumbers++;  
+            }
+            if (parityNumbers >= length-1)
+                return true;
+            else
+                return false;
         }
     }
 }
